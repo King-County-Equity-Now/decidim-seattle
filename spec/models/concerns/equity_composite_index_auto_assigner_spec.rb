9e484640cbde6195cf97bd676f9be15016767be8
Mike Judge <mikelovesrobots@gmail.com>
2020-07-28 17:59:57 -0700

    Clean up the language a bit. It's been a while since I had a deeply nested set of tests

--- spec/models/concerns/equity_composite_index_auto_assigner_spec.rb
+++ spec/models/concerns/equity_composite_index_auto_assigner_spec.rb
@@ -3,62 +3,65 @@ require "decidim/proposals/test/factories"
 
 RSpec.describe 'EquityCompositeIndexAutoAssigner' do
   subject do
-    build(:proposal)
+    create(:proposal)
   end
 
-  describe "when the model is saved" do
-    describe 'and the logitude or latitude has not changed' do
-      it 'does not assign the equity_composite_percentile attribute' do
+  describe "saving" do
+    describe 'when the coordinates have not changed' do
+      it 'does not populate the equity_composite_percentile attribute' do
         subject.save!
         expect(subject.equity_composite_index_percentile).to be_nil
       end
     end
 
-    describe 'when the logitude or latitude is now present' do
+    describe 'when the coordinates are now present' do
       before(:each) do
         subject.latitude = 47.6154049
         subject.longitude = -122.3202612
       end
 
-      describe 'and the EquityComposite record including that point is found' do
+      describe 'and an EquityComposite record including that coordinate was found' do
         before(:each) do
           allow(EquityComposite).to receive_message_chain(:contains_point, :first) do
             build(:equity_composite, composite_index_percentage: 0.459)
           end
         end
 
-        it 'assigns the equity_composite_percentile attribute to the record' do
+        it 'caches equity_composite_index_percentile on the record' do
           subject.save!
           expect(subject.equity_composite_index_percentile).to be(0.459)
         end
       end
 
-      describe 'and the EquityComposite including that point is not found' do
+      describe 'and an EquityComposite including that coordinate was not found' do
         before(:each) do
           allow(EquityComposite).to receive_message_chain(:contains_point, :first) do
             nil
           end
         end
 
-        it 'assigns nil to the equity_composite_percentile attribute in the record' do
+        it 'assigns nil to the equity_composite_index_percentile' do
           subject.save!
           expect(subject.equity_composite_index_percentile).to be_nil
         end
       end
     end
 
-    describe 'when the logitude or latitude is nil' do
+    describe 'when the coordinates are no longer present' do
       subject do
-        build(:proposal)
+        create(:proposal, latitude: 47.6154049, longitude: -122.3202612)
       end
 
       before(:each) do
+        subject.latitude = nil
+        subject.longitude = nil
+
         allow(EquityComposite).to receive_message_chain(:contains_point, :first) do
           nil
         end
       end
 
-      it 'assigns nil to the equity_composite_percentile attribute in the record' do
+      it 'assigns nil to the equity_composite_index_percentile attribute' do
         subject.save!
         expect(subject.equity_composite_index_percentile).to be_nil
       end
