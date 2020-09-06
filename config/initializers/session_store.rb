80e8d669ff74a8dd60c41f4bcc8f0d544a6c171b
Shir Goldberg <me@shirgoldbird.com>
2020-09-06 01:33:40 -0700

    update cookie store name to use "Seattle" instead of "Helsinki"

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_decidim_helsinki_session"
+Rails.application.config.session_store :cookie_store, key: "_decidim_seattle_session"
